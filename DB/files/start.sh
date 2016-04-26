c49e2e8021f4719b31a63b7cd4666ce02029a162
germanblanco <german.blanco@gmail.com>
2016-04-26 15:00:39 +0200

    Add a timestamp in the table.

--- DB/files/start.sh
+++ DB/files/start.sh
@@ -3,6 +3,6 @@
 /entrypoint.sh mysqld &
 sleep 60
 mysql -h$HOSTNAME -uroot -ppasswd --execute="CREATE DATABASE reto1;"
-mysql -h$HOSTNAME -uroot -ppasswd reto1 --execute="CREATE TABLE reto1 (value VARCHAR(100) NOT NULL);"
+mysql -h$HOSTNAME -uroot -ppasswd reto1 --execute="CREATE TABLE reto1 (value VARCHAR(100) NOT NULL, insert_time TIMESTAMP NOT NULL);"
 
 wait %1
