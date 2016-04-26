f61450faf46a15a06aec626b4a1b4fba9f1ef063
germanblanco <german.blanco@gmail.com>
2016-04-26 15:30:02 +0200

    Enable configuration of speed of load test.

--- load/files/start.sh
+++ load/files/start.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
 if [ ! -z ${THESERVER+x} ]; then sed -i.bak s/reto1server/$THESERVER/ /root/.tsung/tsung.xml ; fi
+sed -i.bak s/arrivalrate="10"/arrivalrate="$RATE"/ /root/.tsung/tsung.xml
+sed -i.bak s/to="10"/to="$HITS"/ /root/.tsung/tsung.xml
 
 tsung start
--- load/start.sh
+++ load/start.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
+RATE=${RATE:-10}
+HITS=${HITS:-10}
 if [ -z ${THESERVER+x} ]; then 
-  sudo docker run --link reto1server --name reto1load reto1load
+  sudo docker run --link reto1server -e "RATE=$RATE" -e "HITS=$HITS" --name reto1load reto1load
 else
-  sudo docker run -e "THESERVER=$THESERVER" -p 80 --name reto1server -d reto1server
+  sudo docker run -e "THESERVER=$THESERVER" -e "RATE=$RATE" -e "HITS=$HITS" -p 80 --name reto1server -d reto1server
 fi
